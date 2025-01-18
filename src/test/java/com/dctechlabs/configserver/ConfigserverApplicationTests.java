af87fd4fa6366c7233dfbe0e8426ce3adcdf9b69
Mohamed Safras <awsafras@gmail.com>
2025-01-18 11:39:55 +0530

    initial commit

---
+++ src/test/java/com/dctechlabs/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dctechlabs.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
